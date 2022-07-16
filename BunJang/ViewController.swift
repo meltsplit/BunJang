e1ce64b20531edc6f3c786f370aa7efdcc00108f
meltsplit <seokwoo2000@naver.com>
2022-07-16 12:16:47 +0900

    Initial Commit

---
+++ BunJang/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BunJang
+//
+//  Created by 장석우 on 2022/07/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
