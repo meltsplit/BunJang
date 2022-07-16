c66035a374c2977ac1ae324f9c43fc7deb31ebc1
meltsplit <seokwoo2000@naver.com>
2022-07-16 20:37:15 +0900

    [Add]: Asset 추가

---
+++ BunJang/Configuration/Constant/Image.swift
@@ -0,0 +1,15 @@
+//
+//  Image.swift
+//  BunJang
+//
+//  Created by 장석우 on 2022/07/16.
+//
+
+import UIKit
+
+struct Image{
+    static var login1 = UIImage(named: "login1")!
+    static var login2 = UIImage(named: "login2")!
+    static var login3 = UIImage(named: "login3")!
+    static var login4 = UIImage(named: "login4")!
+}
