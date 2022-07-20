372c40ed0468a908eccb6226e11013db91332fb9
meltsplit <seokwoo2000@naver.com>
2022-07-20 23:18:24 +0900

    [Feat]: 등록 - 카테고리 선택 API연동 완료

---
+++ BunJang/Source/View/Home/TotalMenu/HomeTotalMenuViewController.swift
@@ -0,0 +1,63 @@
+//
+//  HomeTotalMenuViewController.swift
+//  BunJang
+//
+//  Created by 장석우 on 2022/07/20.
+//
+
+import UIKit
+
+class HomeTotalMenuViewController : BaseViewController{
+    
+    //MARK: - IBOutlet
+    
+    //MARK: - Properties
+    
+    
+    //MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setDelegate()
+        setUI()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        hideNavBar()
+        hideTabBar()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        showNavBar()
+        showTabBar()
+        
+    }
+    
+    //MARK: - Custom Method
+    
+    private func setDelegate(){
+        true
+    }
+    
+    private func setUI(){
+        true
+    }
+    
+    
+    //MARK: - IBAction
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+    
+}
+
+extension HomeTotalMenuViewController {
+    
+}
+
